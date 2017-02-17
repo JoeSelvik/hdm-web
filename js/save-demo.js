f0c2c3eb757a3d0f596d78d39064a0100a0b3af6
JoeSelvik <joe@thenewtricks.com>
2017-02-17 09:04:13 -0700

    added sliders to control breacket size

--- js/save-demo.js
+++ js/save-demo.js
@@ -1,9 +1,9 @@
 var saveData = {
   teams: [
     ["Team 1", "Team 2"],
-    ["Team 3", null],
-    ["Team 4", null],
-    ["Team 5", null]
+    ["Team 3", "jim"],
+    ["Team 4", "george"],
+    ["Team 5", "matt"]
   ],
   results: [
       [
@@ -13,6 +13,23 @@ var saveData = {
       ]
   ]
 };
+
+// These are modified by the sliders
+var resizeParameters = {
+  teamWidth: 60,
+  scoreWidth: 20,
+  matchMargin: 10,
+  roundMargin: 50,
+  init: saveData
+};
+ 
+function updateResizeDemo() {
+  $('#save .demo').bracket(resizeParameters);
+}
+ 
+// todo: do I need this here?
+// $(updateResizeDemo)
+
  
 /* Called whenever bracket is modified
  *
@@ -20,7 +37,7 @@ var saveData = {
  * userData: optional data given when bracket is created.
  */
 function saveFn(data, userData) {
-  var json = jQuery.toJSON(data)
+  var json = JSON.stringify(data)
   $('#saveOutput').text('POST '+userData+' '+json)
   /* You probably want to do something like this
   jQuery.ajax("rest/"+userData, {contentType: 'application/json',
@@ -29,15 +46,19 @@ function saveFn(data, userData) {
                                 data: json})
   */
 }
+
  
 $(function() {
     var container = $('div#save .demo')
     container.bracket({
       init: saveData,
       save: saveFn,
-      userData: "http://myapi"})
+      userData: "http://myapi",
+      disableToolbar: true,
+      disableTeamEdit: true
+    })
  
     /* You can also inquiry the current data */
     var data = container.bracket('data')
-    $('#dataOutput').text(jQuery.toJSON(data))
+	$('#dataOutput').text(JSON.stringify(data))
   })
